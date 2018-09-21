15fe8cd83d09e6e6f13b76612081cc017d7cf8f6
Meli093 <31870468+Meli093@users.noreply.github.com>
2018-09-20 19:52:12 -0500

    llevamos una cuarta parte de practicas faltan mas
    vamo bien

---
+++ ManejoDeVariables/public_html/js/programa.js
@@ -0,0 +1,20 @@
+document.write("programa para declaracion de variable:<br/>");
+
+//declaracion de variable
+var x=5;
+
+var y;
+y=6;
+
+var z= x+y;
+
+document.write("valor de x:" +x);
+
+document.write("<br>valor de x:" +y);
+
+document.write("<br>valor de x:" +z);
+
+var a= -5,b=4, c= a+b;
+
+document.write("<br/>");
+document.write("valor de a:"+a+", b"+b+", c:"+c);
